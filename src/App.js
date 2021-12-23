92501dbe6b1048fb8fb0361554fab43f201f84b1
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-12-23 11:46:23 +0300

    create scrollbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
 import './App.css';
 import Sidebar from './components/sidebar/sidebar';
-import Header from './components/content/content';
 import RightMenu from './components/rightMenu/rightMenu';
 import { Routes, Route } from 'react-router-dom';
 import Content from './components/content/content';
+import { useState } from 'react';
+
+
+
 
 function App() {
+
+  const items = [
+    {
+      title: 'Заголовок 1',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 2',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 3',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 4',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 5',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 5',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 5',
+      text: 'Текст'
+    },
+    {
+      title: 'Заголовок 5',
+      text: 'Текст'
+    },
+    
+  ]
+  
+
+
   return (
     <>
         <div className="wrapper">
-          <Content/>
+          <Content _class='items'>
+                {
+                  items.map((v, i) => {
+                    return (
+                      <div key={i} className='item'>
+                            <h2>{v.title}</h2>
+                            <p>{v.text}</p>
+                      </div>
+                    )
+                  })
+                }
+          </Content>
           <RightMenu/>
           {/* <Routes>
           </Routes> */}
--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,11 +1,93 @@
 import './content.css';
 import watch from '../../img/logo/watch.svg';
 import search from '../../img/logo/search.svg';
+import React, { useEffect, useState, useRef } from 'react';
 
 
 
-const Content = () => {
+const Content = (props) => {
+    let ref = useRef()
+    const [state, setState] = useState ({
+        isScrolling: false,
+        clientX: 0,
+        scrollX: 0
+    });
+
+
+    useEffect(() => {
+         const el = ref.current
+         if(el) {
+             const onWheel = e => {
+                 e.preventDefault()
+                 el.scrollTo({
+                     left: el.scrollLeft + e.deltaY * 5,
+                     behavior: 'smooth'
+                 })
+             }
+             el.addEventListener('wheel', onWheel)
+
+             return () => el.removeEventListener('wheel', onWheel)
+         } 
+    }, [])
+
+    const onMouseDown = e => {
+        if (ref && ref.current && !ref.current.contains(e.target)) {
+
+        }
+        e.preventDefault()
+
+        setState ({
+            ...state,
+            isScrolling: true,
+            clientX: e.clientX
+        })
+    }
+    const onMouseUp = e => {
+        if (ref && ref.current && !ref.current.contains(e.target)) {
+            
+        }
+        e.preventDefault()
+
+        setState ({
+            ...state,
+            isScrolling: false,
+        })
+    }
+    const onMouseMove = e => {
+        if (ref && ref.current && !ref.current.contains(e.target)) {
+            
+        } 
+        e.preventDefault()
+
+        const {clientX, scrollX, isScrolling} = state
+
+        if (isScrolling) {
+            ref.current.scrollLeft = scrollX + e.clientX - clientX
+            let sX = scrollX + e.clientX - clientX
+            let cX = e.clientX
+
+            setState({
+                ...state,
+                scrollX: sX,
+                clientX: cX
+            })
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', onMouseDown)
+        document.addEventListener('mouseup', onMouseUp)
+        document.addEventListener('mousemove', onMouseMove)
+
+        return () => {
+            document.removeEventListener('mousedown', onMouseDown)
+            document.removeEventListener('mouseup', onMouseUp)
+            document.removeEventListener('mousemove', onMouseMove)
+        }
+    })
+
     return (
+
         <div className='content'>  
             <div className='contentHeader'>
                 <div className='contentHeaderBox'>
@@ -27,6 +109,20 @@ const Content = () => {
                 </div>  
             </div> 
             <hr className='hr' align="center"></hr> 
+
+            <div className='contentScroll'>
+                <div 
+                ref={ref}
+                className={props._class}
+                onMouseDown={onmousedown}
+                onMouseUp={onmouseup}
+                onMouseMove={onmousemove}
+                >
+                    {
+                        React.Children.map(props.children, child => React.Children.only(child))
+                    }
+                </div>
+            </div>
             
         </div>
     )
