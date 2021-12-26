04d37f40af35645ffd8d99fe54ef31f4096edce7
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-12-26 22:43:57 +0300

    work on footerslider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
+import './index.css';
 import './components/content/slider/slider.css';
 import Sidebar from './components/sidebar/sidebar';
 import RightMenu from './components/rightMenu/rightMenu';
 import Content from './components/content/content'; 
+import Scrollable from './components/content/slider/scrollable';
 
 
 
 
 
 function App() {
+
+  const listItems = [
+    {
+      image: '',
+      title: '',
+      text: ''
+    },
+    {
+      image: '',
+      title: '',
+      text: ''
+    },
+    {
+      image: '',
+      title: '',
+      text: ''
+    },
+    {
+      image: '',
+      title: '',
+      text: ''
+    },
+    {
+      image: '',
+      title: '',
+      text: ''
+    }
+  ]
 
   return (
     <>
@@ -17,8 +47,27 @@ function App() {
           <RightMenu/>
           <Sidebar/>
         </div>
+
+        <div className='container'>
+            <Scrollable _class='listItems'>
+                {
+                  listItems.map((v, i) => {
+                    return (
+                      <div key={i} className='scrollableItem'>
+                          <h2>{v.title}</h2>
+                          <p>{v.text}</p>
+                      </div>
+                    )
+                  })
+                }
+            </Scrollable>
+        </div>
+
+      
     </>
+    
   );
+
 }
 
 export default App;
