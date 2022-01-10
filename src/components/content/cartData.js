fe7b954c99a9610989e68b6f1848831b6d7e882a
Rustdj <81041717+Rustdj@users.noreply.github.com>
2022-01-10 11:20:42 +0300

    create dinamic add on the hooks

--- src/components/content/cartData.js
+++ src/components/content/cartData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import fishPizza from '../../img/pizza/formaggio.svg';
 import '../content/cartData.css';
 import '../content/set/set.css';
@@ -6,6 +6,23 @@ import '../content/footer/footer.css';
 import '../content/content.css';
 
 const CartData = () => {
+
+    const [slide, setSlide] = useState(0);
+    const decCounter = () => {
+        if ( slide > 0) {
+            setSlide(slider => slider -1)
+        }
+    }
+
+    function changeSlide(i) {
+        setSlide(slide + i);
+    }
+
+    useEffect(() => {
+        console.log('effect');
+        document.title = `Slide: ${slide}`;
+    }, [slide]);
+
     return (
         <div className='cartWrapper'>
             <div className='setName'>
@@ -19,14 +36,14 @@ const CartData = () => {
                     <h2>Пицца Новогодняя с лососем и красной икрой</h2>
                             <p>Тесто, соус сливочный, лосось, икра лосося, сыр моцарелла, руккола</p>
                         <div className='cartSumm'>
-                            <div>770 ₽</div>
+                            <div className='price'>770 ₽</div>
                             <div className='cartAdd'>
-                                <button>&#43;</button> 
-                                <div className='num'>0</div>
-                                <button>&#8722;</button>  
+                                <button onClick={() => decCounter(-1)}>&#8722;</button> 
+                                <div className='num'>{slide}</div>
+                                <button onClick={() => changeSlide(1)}>&#43;</button> 
                             </div>                
                         </div>
-                        <button>Хочу!</button>
+                        <button className="btnCart">Хочу!</button>
                     </div>
                 </div>
         </div>
